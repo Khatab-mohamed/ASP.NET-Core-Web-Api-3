f8f4c198960e78f0d3117adf755f3a0f82478cd4
Dev_khatab <Dev_khatab@DESKTOP-VG2PC12>
2020-04-07 04:09:05 -0700

    Returning  NotFound for unsupported Media Type

--- CourseLibrary.API/Startup.cs
+++ CourseLibrary.API/Startup.cs
@@ -21,6 +21,8 @@ namespace CourseLibrary.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers(setupAction =>
+                setupAction.ReturnHttpNotAcceptable = true);
             services.AddControllers();
 
             services.AddScoped<ICourseLibraryRepository, CourseLibraryRepository>();
